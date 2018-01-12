e0831758fe0a510091f00abd13fbb39fdc20beaf
dereuromark <mark.scherer@spryker.com>
2018-01-12 20:17:31 +0100

    Add delete functionality.

---
+++ tests/TestCase/Controller/Admin/FeedbackControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Feedback\Test\TestCase\Controller\Admin;
+
+use Cake\Core\Configure;
+use Cake\TestSuite\IntegrationTestCase;
+use Feedback\Store\FilesystemStore;
+
+class FeedbackControllerTest extends IntegrationTestCase {
+
+	/**
+	 * @return void
+	 */
+	public function setUp() {
+		Configure::write('Feedback', [
+			'configuration' => [
+				FilesystemStore::NAME => [
+					'location' => TMP,
+				],
+			],
+		]);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testRemove() {
+		$file = time() . '-' . session_id() . '.feedback';
+		$savepath = Configure::read('Feedback.configuration.Filesystem.location');
+		$data = [
+			'screenshot' => '123',
+		];
+		file_put_contents($savepath . $file, serialize($data));
+		$this->assertFileExists($savepath . $file);
+
+		$this->post(['prefix' => 'admin', 'plugin' => 'Feedback', 'controller' => 'Feedback', 'action' => 'remove', $file]);
+
+		$this->assertResponseCode(302);
+		$this->assertRedirect(['prefix' => 'admin', 'plugin' => 'Feedback', 'controller' => 'Feedback', 'action' => 'index']);
+
+		$this->assertFileNotExists($savepath . $file);
+	}
+
+}
