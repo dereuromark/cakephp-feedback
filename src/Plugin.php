113ce2c9186100765b343f754cf7776a5fe5caa3
mscherer <mark.scherer@spryker.com>
2020-03-17 12:14:53 +0100

    Disable hooks.

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,6 +8,21 @@ use Cake\Routing\RouteBuilder;
 class Plugin extends BasePlugin {
 
 	/**
+	 * @var bool
+	 */
+	protected $middlewareEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $consoleEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $bootstrapEnabled = false;
+
+	/**
 	 * @param \Cake\Routing\RouteBuilder $routes The route builder to update.
 	 * @return void
 	 */
