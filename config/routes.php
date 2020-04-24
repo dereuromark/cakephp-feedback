960ab0f55bc6345a567b90f940cba2828e24d963
mscherer <mark.scherer@spryker.com>
2020-04-24 03:24:51 +0200

    Fix routes

--- config/routes.php
+++ config/routes.php
@@ -3,7 +3,7 @@
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
-Router::prefix('admin', function (RouteBuilder $routes) {
+Router::prefix('Admin', function (RouteBuilder $routes) {
 	$routes->plugin('Feedback', function (RouteBuilder $routes) {
 		$routes->connect('/', ['controller' => 'Feedback', 'action' => 'index']);
 
